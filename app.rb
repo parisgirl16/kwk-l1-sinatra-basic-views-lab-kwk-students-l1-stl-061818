c6ace065e956e3b4360695a99c43999af2499c77
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-26 21:36:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,29 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+get '/' do
+  "Bowling Alley:
+  For hours, add '/hours',
+  For dates of tournaments, add '/tournaments',
+  For bar menus, add '/menus'"
+end
 
+get '/hours' do
+  "Bowling Alley hours:
+  Monday-Saturday: 9am-10pm,
+  Sunday: 9am-4pm"
+end
+
+get '/tournaments' do
+  "Bowling Tournament Dates:
+  Every Thursday from June 7 to August 30"
+end
+
+get '/menus' do
+  "Bar Menu:
+  Beer,
+  Wine,
+  Cocktails,
+  Non-alcoholic drinks for the kids, too"
+end
 end
